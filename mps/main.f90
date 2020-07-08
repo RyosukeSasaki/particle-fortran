fcc1d4238116cc1f30be098f255dea2c380b4dc9
RyosukeSasaki <rsasaki@keio.jp>
2020-07-08 19:50:35 +0900

    edit gitignore

--- mps/main.f90
+++ mps/main.f90
@@ -9,27 +9,27 @@ program main
   call calcConsts()
   do i = 1, 200
   do j = 1, 10
-  call calcGravity()
-  call calcViscosity()
-  call moveParticleExplicit()
-  call collision()
-  call calcNumberDensity()
-  call setBoundaryCondition()
-  call setSourceTerm()
-  call setMatrix()
-  call GaussEliminateMethod()
-  call removeNegativePressure()
-  call setMinPressure()
-  call calcPressureGradient()
-  call moveParticleImplicit()
+    call calcGravity()
+    call calcViscosity()
+    call moveParticleExplicit()
+    call collision()
+    call calcNumberDensity()
+    call setBoundaryCondition()
+    call setSourceTerm()
+    call setMatrix()
+    call GaussEliminateMethod()
+    call removeNegativePressure()
+    call setMinPressure()
+    call calcPressureGradient()
+    call moveParticleImplicit()
   enddo
   write (*, *) "Timestep: ", i*10
   write (filename, '("data/data", i4.4, ".dat")') i
   open (11, file=filename, status='replace')
   do k = 1, NumberOfParticle
-  write (11, *) Pos(k, 1), Pos(k, 2), ParticleType(k)
+    write (11, *) Pos(k, 1), Pos(k, 2), ParticleType(k), Pressure(i)
   enddo
   close (11)
   enddo
 
-  end
+end
